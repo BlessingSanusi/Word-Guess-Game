b2825526bba67a110cd374ec4bbce3eeb0f0b188
Blessing Sanusi <sanusi.temitope@ymail.com>
2019-07-01 17:59:16 -0500

    pushing codes

---
+++ assets/javascript/game.js
@@ -0,0 +1,95 @@
+//arrays of musicians name
+const names = [
+  "Beyonce",
+  "Eminem",
+  "Ludacris",
+  "Shakira",
+  "Kanye",
+  "Nelly",
+  "Rihanna",
+  "Outkast",
+  "Coldplay",
+  "Adele",
+  "Drake",
+  "Madonna",
+  "Usher",
+  "Fergie",
+  "Ashanti",
+  "Timbaland",
+  "Fantasia",
+  "Maxwell"
+];
+
+let wordLetters = document.getElementById("currentWord");
+let wrongletter = document.getElementById("wrongLetters");
+let GuessRemains = document.getElementById("remainingGuess");
+let correctWords = document.getElementById("totalWins");
+let wrongWords = document.getElementById("totalLoss");
+let maxLives = 12;
+let letterDash;
+let incorrectLetter;
+let rightLetter;
+let wins = 0;
+let losses = 0;
+let start = document.getElementById("startGame");
+start.textContent = "Begin by pressing any key";
+//game
+//game function{
+function celebrityGame() {
+  letterDash = [];
+  incorrectLetter = [];
+  maxLives = 12;
+  computerRandPick = names[
+    Math.floor(Math.random() * names.length)
+  ].toLowerCase();
+  chosenWord = computerRandPick.split("");
+
+  for (var i = 0; i < chosenWord.length; i++) {
+    letterDash[i] = "_";
+  }
+  wordLetters.textContent = letterDash.join(" ");
+  wrongletter.textContent = "Wrong Letters: ";
+  GuessRemains.textContent = "Lives left: " + maxLives;
+  correctWords.textContent = "Wins: " + wins;
+  wrongWords.textContent = "Losses: " + losses;
+  beginGame();
+}
+
+function beginGame() {
+  document.onkeyup = function(event) {
+    let userGuess = event.key;
+    start.textContent = "";
+
+    rightLetter = chosenWord.includes(userGuess);
+    if (rightLetter) {
+      for (var j = 0; j < computerRandPick.length; j++) {
+        var comPick = computerRandPick[j];
+        if (comPick === userGuess) letterDash[j] = comPick;
+        wordLetters.textContent = letterDash.join(" ");
+        score();
+      }
+    } else if (incorrectLetter.indexOf(userGuess) === -1) {
+      incorrectLetter.push(userGuess);
+      maxLives--;
+      score();
+    }
+
+    wrongletter.textContent = "wrong letter " + incorrectLetter.toString();
+    GuessRemains.textContent = "lives remaining " + maxLives;
+  };
+}
+
+function score() {
+  if (chosenWord.toString() === letterDash.toString()) {
+    wins++;
+    start.textContent = `Congratulations you got ${computerRandPick} right`;
+    celebrityGame();
+  } else if (maxLives === 0) {
+    losses++;
+    start.textContent = `Sorry, you got ${computerRandPick} wrong`;
+    celebrityGame();
+  }
+}
+
+celebrityGame();
+score();
